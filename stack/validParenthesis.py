f0a3405366b5749115a8036216a0db14c50abbe2
parthpatelz <49402258+parthpatelz@users.noreply.github.com>
2024-10-29 20:44:23 -0400

    basketball, rev polish, valid paranthesis,temp

---
+++ stack/validParenthesis.py
@@ -0,0 +1,62 @@
+# 20. Valid Parentheses
+# Easy
+# Topics
+# Companies
+# Hint
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+# An input string is valid if:
+
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Every close bracket has a corresponding open bracket of the same type.
+
+
+# Example 1:
+
+# Input: s = "()"
+
+# Output: true
+
+# Example 2:
+
+# Input: s = "()[]{}"
+
+# Output: true
+
+# Example 3:
+
+# Input: s = "(]"
+
+# Output: false
+
+# Example 4:
+
+# Input: s = "([])"
+
+# Output: true
+
+
+# Constraints:
+
+# 1 <= s.length <= 104
+# s consists of parentheses only '()[]{}'.
+
+def isValid():
+    s = "()[]{}"
+    stk=[]
+    hmap={')':'(', ']':'[', '}':'{'}
+
+    for i in s:
+        if i not in hmap:
+            stk.append(i)
+        else:
+            if not stk:
+                return False
+            else:
+                poped=stk.pop()
+                if poped!= hmap[i]:
+                    return False
+    return not stk
+
+print(isValid())
